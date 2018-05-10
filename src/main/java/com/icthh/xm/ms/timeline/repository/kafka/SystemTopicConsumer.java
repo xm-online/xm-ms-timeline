6424963b9d9c27aee7684490c645580d76aa9047
emonastyrev <emonastyrev@jbs.com.ua>
2018-05-10 15:09:27 +0300

    add kafka listener

--- src/main/java/com/icthh/xm/ms/timeline/repository/kafka/SystemTopicConsumer.java
+++ src/main/java/com/icthh/xm/ms/timeline/repository/kafka/SystemTopicConsumer.java
@@ -2,25 +2,35 @@ package com.icthh.xm.ms.timeline.repository.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.icthh.xm.commons.config.client.repository.ConfigurationModel;
+import com.icthh.xm.commons.config.domain.Configuration;
 import com.icthh.xm.commons.logging.util.MdcUtils;
+import com.icthh.xm.commons.messaging.event.system.SystemEvent;
+import com.icthh.xm.commons.messaging.event.system.SystemEventType;
 import com.icthh.xm.ms.timeline.config.Constants;
-import com.icthh.xm.ms.timeline.domain.SystemEvent;
 import com.icthh.xm.ms.timeline.service.tenant.KafkaService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
 public class SystemTopicConsumer {
 
     private final KafkaService kafkaService;
+    private final ConfigurationModel configurationModel;
+
+    public SystemTopicConsumer(KafkaService kafkaService, Optional<ConfigurationModel> configurationModel) {
+        this.kafkaService = kafkaService;
+        this.configurationModel = configurationModel.orElse(null);
+    }
 
     /**
      * Consume tenant command event message.
@@ -41,14 +51,17 @@ public class SystemTopicConsumer {
                 log.info("Process event from topic [{}], type='{}', source='{}', event_id ='{}'",
                     message.topic(), event.getEventType(), event.getMessageSource(), event.getEventId());
 
-                String tenant = event.getData().get(Constants.EVENT_TENANT);
+                String tenant = Objects.toString(event.getDataMap().get(Constants.EVENT_TENANT), null);
                 String command = event.getEventType();
                 switch (command.toUpperCase()) {
-                    case Constants.CREATE_COMMAND:
+                    case SystemEventType.CREATE_COMMAND:
                         kafkaService.createKafkaConsumer(tenant);
                         break;
-                    case Constants.DELETE_COMMAND:
+                    case SystemEventType.DELETE_COMMAND:
                         kafkaService.deleteKafkaConsumer(tenant);
+                        break;
+                    case SystemEventType.SAVE_CONFIGURATION:
+                        onSaveConfiguration(event);
                         break;
                     default:
                         log.info("Event ignored with type='{}', source='{}', event_id='{}'",
@@ -63,5 +76,17 @@ public class SystemTopicConsumer {
         } finally {
             MdcUtils.removeRid();
         }
+    }
+
+    private void onSaveConfiguration(SystemEvent event) {
+        String path = Objects.toString(event.getDataMap().get("path"), null);
+        if (StringUtils.isBlank(path)) {
+            throw new IllegalArgumentException("Event '" + event.getEventType() + "' configuration path can't be blank");
+        }
+        String commit = Objects.toString(event.getDataMap().get("commit"), null);
+        if (StringUtils.isBlank(commit)) {
+            throw new IllegalArgumentException("Event '" + event.getEventType() + "' configuration commit can't be blank");
+        }
+        configurationModel.updateConfiguration(new Configuration(path, null, commit));
     }
 }
