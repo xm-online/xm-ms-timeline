9e4e4437757ac27c68703de1f830edc4efd33087
Andrew Koidan <deathangel908@gmail.com>
2018-12-06 17:11:02 +0200

    Added liquibase

--- src/main/java/com/icthh/xm/ms/timeline/TimelineApp.java
+++ src/main/java/com/icthh/xm/ms/timeline/TimelineApp.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.*;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
@@ -31,7 +32,7 @@ import java.util.Collection;
         classes = OAuth2InterceptedFeignConfiguration.class)
 )
 @EnableAutoConfiguration(exclude = {MetricFilterAutoConfiguration.class, MetricRepositoryAutoConfiguration.class})
-@EnableConfigurationProperties({ApplicationProperties.class})
+@EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
 @EnableDiscoveryClient
 public class TimelineApp {
 
