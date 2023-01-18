6a7285f8cb093164add5ebe47f8bd45a09bd9f84
emurenko <murenko-ye@mc.gcf>
2023-01-18 18:01:42 +0200

    update

--- src/main/java/com/icthh/xm/ms/timeline/config/TopicManagerConfiguration.java
+++ src/main/java/com/icthh/xm/ms/timeline/config/TopicManagerConfiguration.java
@@ -1,29 +1,15 @@
 package com.icthh.xm.ms.timeline.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.icthh.xm.commons.config.client.repository.TenantListRepository;
-import com.icthh.xm.commons.logging.trace.SleuthWrapper;
 import com.icthh.xm.commons.security.XmAuthenticationContextHolder;
 import com.icthh.xm.commons.tenant.TenantContextHolder;
-import com.icthh.xm.commons.topic.message.LepMessageHandler;
 import com.icthh.xm.commons.topic.message.MessageHandler;
-import com.icthh.xm.commons.topic.message.MessageService;
-import com.icthh.xm.commons.topic.service.DynamicConsumerConfiguration;
-import com.icthh.xm.commons.topic.service.DynamicConsumerConfigurationService;
-import com.icthh.xm.commons.topic.service.TopicConfigurationService;
-import com.icthh.xm.commons.topic.service.TopicManagerService;
 import com.icthh.xm.lep.api.LepManager;
 import com.icthh.xm.ms.timeline.service.DomainEventService;
 import com.icthh.xm.ms.timeline.service.kafka.DomainEventMessageHandler;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -31,52 +17,12 @@ public class TopicManagerConfiguration {
 
     private final ObjectMapper objectMapper;
     private final DomainEventService domainEventService;
-    private final MessageService messageService;
     private final TenantContextHolder tenantContextHolder;
     private final XmAuthenticationContextHolder authContextHolder;
     private final LepManager lepManager;
 
     @Bean
-    public TopicConfigurationService topicConfigurationService(@Value("${spring.application.name}") String appName,
-                                                               KafkaProperties kafkaProperties,
-                                                               KafkaTemplate<String, String> kafkaTemplate,
-                                                               SleuthWrapper sleuthWrapper,
-                                                               TenantListRepository tenantListRepository) {
-        List<DynamicConsumerConfiguration> dynamicConsumerConfigurations = new ArrayList<>();
-
-        TopicConfigurationService topicConfigurationService = new TopicConfigurationService(appName,
-            dynamicConsumerConfigurationService(kafkaProperties, kafkaTemplate, sleuthWrapper, tenantListRepository, dynamicConsumerConfigurations),
-            consumerMessageHandler());
-
-        dynamicConsumerConfigurations.add(topicConfigurationService);
-
-        return topicConfigurationService;
-    }
-
-    @Bean
-    public DynamicConsumerConfigurationService dynamicConsumerConfigurationService(KafkaProperties kafkaProperties,
-                                                                                   KafkaTemplate<String, String> kafkaTemplate,
-                                                                                   SleuthWrapper sleuthWrapper,
-                                                                                   TenantListRepository tenantListRepository,
-                                                                                   List<DynamicConsumerConfiguration> dynamicConsumerConfigurations) {
-        return new DynamicConsumerConfigurationService(dynamicConsumerConfigurations,
-            topicManagerService(kafkaProperties, kafkaTemplate, sleuthWrapper), tenantListRepository);
-    }
-
-    @Bean
-    public TopicManagerService topicManagerService(KafkaProperties kafkaProperties,
-                                                   KafkaTemplate<String, String> kafkaTemplate,
-                                                   SleuthWrapper sleuthWrapper) {
-        return new TopicManagerService(kafkaProperties, kafkaTemplate, sleuthWrapper);
-    }
-
-    @Bean
-    public MessageHandler consumerMessageHandler() {
+    public MessageHandler messageHandler() {
         return new DomainEventMessageHandler(objectMapper, domainEventService, tenantContextHolder, authContextHolder, lepManager);
-    }
-
-    @Bean
-    public MessageHandler lepMessageHandler() {
-        return new LepMessageHandler(messageService, tenantContextHolder, authContextHolder, lepManager);
     }
 }
