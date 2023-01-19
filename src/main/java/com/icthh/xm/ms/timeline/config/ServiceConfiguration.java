740e57bed4ea862352c6fe8abe4ebbd5295371e8
emurenko <murenko-ye@mc.gcf>
2023-01-19 18:03:24 +0200

    removed matchIfMissing and default TimelineService implementation

--- src/main/java/com/icthh/xm/ms/timeline/config/ServiceConfiguration.java
+++ src/main/java/com/icthh/xm/ms/timeline/config/ServiceConfiguration.java
@@ -1,9 +1,5 @@
 package com.icthh.xm.ms.timeline.config;
 
-import static com.icthh.xm.ms.timeline.config.Constants.CASSANDRA_IMPL;
-import static com.icthh.xm.ms.timeline.config.Constants.LOGGER_IMPL;
-import static com.icthh.xm.ms.timeline.config.Constants.RDBMS_IMPL;
-
 import com.icthh.xm.commons.tenant.TenantContextHolder;
 import com.icthh.xm.ms.timeline.repository.cassandra.EntityMappingRepository;
 import com.icthh.xm.ms.timeline.repository.cassandra.TimelineCassandraRepository;
@@ -15,10 +11,13 @@ import com.icthh.xm.ms.timeline.service.cassandra.TimelineServiceCassandraImpl;
 import com.icthh.xm.ms.timeline.service.db.TimelineServiceDbImpl;
 import com.icthh.xm.ms.timeline.service.logger.TimelineServiceLoggerImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static com.icthh.xm.ms.timeline.config.Constants.CASSANDRA_IMPL;
+import static com.icthh.xm.ms.timeline.config.Constants.LOGGER_IMPL;
+import static com.icthh.xm.ms.timeline.config.Constants.RDBMS_IMPL;
 
 @Configuration
 @RequiredArgsConstructor
@@ -45,14 +44,5 @@ public class ServiceConfiguration {
     @ConditionalOnProperty(name = "application.timeline-service-impl", havingValue = RDBMS_IMPL)
     public TimelineService dbTimelineService(TimelineJpaRepository timelineJpaRepository) {
         return new TimelineServiceDbImpl(timelineJpaRepository, tenantPropertiesService, sortProcessor);
-    }
-
-    @Bean(name = "timelineService")
-    @ConditionalOnProperty(name = "application.timeline-service-impl", matchIfMissing = true)
-    @ConditionalOnMissingBean
-    public TimelineService defaultTimelineService(TimelineCassandraRepository tr,
-                                                  EntityMappingRepository emr,
-                                                  TenantContextHolder tch) {
-        return new TimelineServiceCassandraImpl(tr, emr, tch);
     }
 }
