ab27fe3d5c05041307bf23653277ca40d6d16b85
emurenko <murenko-ye@mc.gcf>
2023-01-24 13:51:47 +0200

    added lep processing

--- src/main/java/com/icthh/xm/ms/timeline/service/DomainEventService.java
+++ src/main/java/com/icthh/xm/ms/timeline/service/DomainEventService.java
@@ -1,6 +1,9 @@
 package com.icthh.xm.ms.timeline.service;
 
 import com.icthh.xm.commons.domainevent.domain.DomainEvent;
+import com.icthh.xm.commons.lep.LogicExtensionPoint;
+import com.icthh.xm.commons.lep.spring.LepService;
+import com.icthh.xm.ms.timeline.lep.keyresolver.DomainEventAggregateTypeResolver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,10 +11,12 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@LepService(group = "topic")
 public class DomainEventService {
 
     private final TimelineService timelineService;
 
+    @LogicExtensionPoint(value = "ProcessEvent", resolver = DomainEventAggregateTypeResolver.class)
     public void processEvent(DomainEvent domainEvent) {
         log.debug("processEvent {}", domainEvent);
         timelineService.insertTimelines(domainEvent);
