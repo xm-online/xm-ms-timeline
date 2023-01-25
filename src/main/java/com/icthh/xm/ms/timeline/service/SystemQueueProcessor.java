a1959b5b65d3f5101f529abd148671a5e65d7f9e
tsavitskyy <taras.savitskyy@jevera.software>
2023-01-25 10:02:05 +0200

    fix

--- src/main/java/com/icthh/xm/ms/timeline/service/SystemQueueProcessor.java
+++ src/main/java/com/icthh/xm/ms/timeline/service/SystemQueueProcessor.java
@@ -2,7 +2,9 @@ package com.icthh.xm.ms.timeline.service;
 
 import com.icthh.xm.commons.lep.spring.LepService;
 import com.icthh.xm.commons.messaging.event.system.SystemEvent;
+import org.springframework.stereotype.Service;
 
+@Service
 @LepService(group = "systemEvent")
 public interface SystemQueueProcessor {
 
