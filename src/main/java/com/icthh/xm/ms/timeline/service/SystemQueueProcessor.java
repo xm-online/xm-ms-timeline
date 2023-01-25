9c646ed7530e8ac1fccca33ff45eb5b6c6cd69f3
tsavitskyy <taras.savitskyy@jevera.software>
2023-01-25 09:52:47 +0200

    added LepService annotation

--- src/main/java/com/icthh/xm/ms/timeline/service/SystemQueueProcessor.java
+++ src/main/java/com/icthh/xm/ms/timeline/service/SystemQueueProcessor.java
@@ -1,9 +1,10 @@
 package com.icthh.xm.ms.timeline.service;
 
+import com.icthh.xm.commons.lep.spring.LepService;
 import com.icthh.xm.commons.messaging.event.system.SystemEvent;
 
+@LepService(group = "systemEvent")
 public interface SystemQueueProcessor {
 
     void processSystemEvent(SystemEvent systemEvent);
-
 }
