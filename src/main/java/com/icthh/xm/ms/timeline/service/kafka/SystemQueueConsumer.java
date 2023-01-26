7b515204dadb94cacb0963ceb26044b95e0fd7c6
emurenko <murenko-ye@mc.gcf>
2023-01-26 19:32:13 +0200

    added init tenant

--- src/main/java/com/icthh/xm/ms/timeline/service/kafka/SystemQueueConsumer.java
+++ src/main/java/com/icthh/xm/ms/timeline/service/kafka/SystemQueueConsumer.java
@@ -4,13 +4,21 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.icthh.xm.commons.logging.util.MdcUtils;
 import com.icthh.xm.commons.messaging.event.system.SystemEvent;
+import com.icthh.xm.commons.security.XmAuthenticationContextHolder;
+import com.icthh.xm.commons.tenant.TenantContextHolder;
+import com.icthh.xm.commons.tenant.TenantContextUtils;
+import com.icthh.xm.lep.api.LepManager;
 import com.icthh.xm.ms.timeline.service.SystemQueueProcessorService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+
+import static com.icthh.xm.commons.lep.XmLepConstants.THREAD_CONTEXT_KEY_AUTH_CONTEXT;
+import static com.icthh.xm.commons.lep.XmLepConstants.THREAD_CONTEXT_KEY_TENANT_CONTEXT;
 
 
 @Slf4j
@@ -20,6 +28,10 @@ public class SystemQueueConsumer {
 
     private final ObjectMapper objectMapper;
     private final SystemQueueProcessorService systemQueueProcessorService;
+
+    private final TenantContextHolder tenantContextHolder;
+    private final XmAuthenticationContextHolder authContextHolder;
+    private final LepManager lepManager;
 
     /**
      * Consume tenant command event message.
@@ -33,10 +45,28 @@ public class SystemQueueConsumer {
         MdcUtils.putRid();
         try {
             SystemEvent event = fromJson(message.value());
+            init(event.getTenantKey());
             systemQueueProcessorService.handleSystemEvent(event);
         } finally {
-            MdcUtils.removeRid();
+            destroy();
         }
+    }
+
+    private void init(String tenantKey) {
+        if (StringUtils.isNotBlank(tenantKey)) {
+            TenantContextUtils.setTenant(tenantContextHolder, tenantKey);
+
+            lepManager.beginThreadContext(threadContext -> {
+                threadContext.setValue(THREAD_CONTEXT_KEY_TENANT_CONTEXT, tenantContextHolder.getContext());
+                threadContext.setValue(THREAD_CONTEXT_KEY_AUTH_CONTEXT, authContextHolder.getContext());
+            });
+        }
+    }
+
+    private void destroy() {
+        lepManager.endThreadContext();
+        tenantContextHolder.getPrivilegedContext().destroyCurrentContext();
+        MdcUtils.removeRid();
     }
 
     private SystemEvent fromJson(String value) {
