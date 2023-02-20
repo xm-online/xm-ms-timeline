7f07a59d337207565f01495fa2523c919ab4f7de
Fedir Pyshnyi <fpyshnyi@itsmartflex.com>
2023-02-20 16:21:30 +0200

    Fix comment (check field aggregateId before parsing)

--- src/main/java/com/icthh/xm/ms/timeline/service/mapper/XmTimelineMapper.java
+++ src/main/java/com/icthh/xm/ms/timeline/service/mapper/XmTimelineMapper.java
@@ -3,15 +3,20 @@ package com.icthh.xm.ms.timeline.service.mapper;
 import com.icthh.xm.ms.timeline.domain.XmTimeline;
 import com.icthh.xm.ms.timeline.service.dto.Timeline;
 import com.icthh.xm.ms.timeline.service.dto.TimelineEvent;
+import org.h2.util.StringUtils;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.Named;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @Mapper(config = BaseMapper.class)
 public interface XmTimelineMapper {
+
+    Logger LOGGER = LoggerFactory.getLogger(XmTimelineMapper.class);
 
     @Mappings({
         @Mapping(target = "aggregateId", expression = "java(timelineEvent.entityId() == null ? null : String.valueOf(timelineEvent.entityId()))"),
@@ -39,13 +44,19 @@ public interface XmTimelineMapper {
 
     @Named("aggregateIdToEntityId")
     default Long aggregateIdToEntityId(String aggregateId) {
-        if (aggregateId != null) {
+        if (aggregateId == null) {
+            return null;
+        }
+
+        if (StringUtils.isNumber(aggregateId)) {
             try {
                 return Long.parseLong(aggregateId);
             } catch (NumberFormatException e) {
+                LOGGER.error("Error parse aggregateId", e);
                 return null;
             }
         } else {
+            LOGGER.debug("The field aggregateId is not a number");
             return null;
         }
     }
