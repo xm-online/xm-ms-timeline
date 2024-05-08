966052fd1495d7baa9fb47fd2528ca17f91c70de
valentyna.osiievska <valentyna.osiievska@dia.com.ua>
2024-05-08 16:56:24 +0300

    DA-24 updated DomainEventAggregateTypeResolver

--- src/main/java/com/icthh/xm/ms/timeline/lep/keyresolver/DomainEventAggregateTypeResolver.java
+++ src/main/java/com/icthh/xm/ms/timeline/lep/keyresolver/DomainEventAggregateTypeResolver.java
@@ -5,19 +5,16 @@ import com.icthh.xm.lep.api.LepKeyResolver;
 import com.icthh.xm.lep.api.LepMethod;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class DomainEventAggregateTypeResolver implements LepKeyResolver {
 
     @Override
     public List<String> segments(LepMethod method) {
-        return Arrays.stream(method.getMethodArgValues())
-            .filter(arg -> arg instanceof DomainEvent)
-            .map(arg -> ((DomainEvent) arg).getAggregateType())
-            .toList();
+        return List.of(
+            method.getParameter("domainEvent", DomainEvent.class).getAggregateType()
+        );
     }
 
 }
