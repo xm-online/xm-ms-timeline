85e11b1c81c858db3062537536df534c8f821938
Valentyna Osiievska <36424928+osiievska23@users.noreply.github.com>
2025-08-08 15:01:48 +0300

    updated commons version tp 4.0.48 (#94)
    * updated commons version tp 4.0.48
    
    * autoincrement project version
    
    * updated commons version tp 4.0.48
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- src/test/java/com/icthh/xm/ms/timeline/config/IntegrationTestConfiguration.java
+++ src/test/java/com/icthh/xm/ms/timeline/config/IntegrationTestConfiguration.java
@@ -7,7 +7,6 @@ import com.icthh.xm.commons.domainevent.service.impl.KafkaTransactionSynchroniza
 import com.icthh.xm.commons.security.jwt.TokenProvider;
 import com.icthh.xm.commons.security.oauth2.JwtVerificationKeyClient;
 import com.icthh.xm.commons.web.spring.TenantVerifyInterceptor;
-import io.prometheus.client.CollectorRegistry;
 import lombok.SneakyThrows;
 import org.springframework.cloud.client.loadbalancer.RestTemplateCustomizer;
 import org.springframework.context.ApplicationContext;
@@ -35,11 +34,6 @@ public class IntegrationTestConfiguration {
         tenants.add("DEMO");
         tenants.add("TEST");
         tenants.add("RESINTTEST");
-    }
-
-    @Bean
-    public CollectorRegistry collectorRegistry() {
-        return new CollectorRegistry();
     }
 
     @Bean
