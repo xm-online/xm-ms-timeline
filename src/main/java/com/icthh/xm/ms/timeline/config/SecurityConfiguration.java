a079ef8e630479f3f69d6015b37e3b1d222dd95c
viktorpavlenko <vitek.pavfurious@gmail.com>
2026-03-20 16:47:29 +0200

    XM3-220: Add security chain filter for api;

---
+++ src/main/java/com/icthh/xm/ms/timeline/config/SecurityConfiguration.java
@@ -0,0 +1,66 @@
+package com.icthh.xm.ms.timeline.config;
+
+import com.icthh.xm.commons.permission.access.XmPermissionEvaluator;
+import com.icthh.xm.commons.security.RoleConstant;
+import com.icthh.xm.commons.security.jwt.JWTConfigurer;
+import com.icthh.xm.commons.security.jwt.TokenProvider;
+import com.icthh.xm.commons.security.spring.config.UnauthorizedEntryPoint;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableMethodSecurity(securedEnabled = true)
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+
+    private final TokenProvider tokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.
+                        frameOptions(HeadersConfigurer.FrameOptionsConfig::deny)
+                )
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/**").authenticated()
+                                .requestMatchers("/management/health").permitAll()
+                                .requestMatchers("/management/prometheus/**").permitAll()
+                                .requestMatchers("/management/**").hasAuthority(RoleConstant.SUPER_ADMIN)
+                                .requestMatchers("/v3/api-docs/**").hasAuthority(RoleConstant.SUPER_ADMIN)
+                                .requestMatchers("/swagger-resources/configuration/ui").permitAll()
+                );
+        http.with(securityConfigurerAdapter(), Customizer.withDefaults());
+        http.exceptionHandling(handler -> handler.authenticationEntryPoint(new UnauthorizedEntryPoint()));
+        return http.build();
+    }
+
+    private JWTConfigurer securityConfigurerAdapter() {
+        return new JWTConfigurer(tokenProvider);
+    }
+
+    @Primary
+    @Bean
+    static MethodSecurityExpressionHandler expressionHandler(XmPermissionEvaluator customPermissionEvaluator) {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+        expressionHandler.setPermissionEvaluator(customPermissionEvaluator);
+        return expressionHandler;
+    }
+}
--- src/main/java/com/icthh/xm/ms/timeline/config/ServiceConfiguration.java
+++ src/main/java/com/icthh/xm/ms/timeline/config/ServiceConfiguration.java
@@ -3,7 +3,6 @@ package com.icthh.xm.ms.timeline.config;
 import static com.icthh.xm.ms.timeline.config.Constants.LOGGER_IMPL;
 import static com.icthh.xm.ms.timeline.config.Constants.RDBMS_IMPL;
 
-import com.icthh.xm.commons.permission.access.XmPermissionEvaluator;
 import com.icthh.xm.ms.timeline.repository.jpa.TimelineJpaRepository;
 import com.icthh.xm.ms.timeline.service.SortProcessor;
 import com.icthh.xm.ms.timeline.service.TenantPropertiesService;
@@ -15,9 +14,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
-import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 
 @Configuration
 @RequiredArgsConstructor
@@ -37,13 +33,5 @@ public class ServiceConfiguration {
     @ConditionalOnProperty(name = "application.timeline-service-impl", havingValue = RDBMS_IMPL)
     public TimelineService dbTimelineService(TimelineJpaRepository timelineJpaRepository) {
         return new TimelineServiceDbImpl(timelineJpaRepository, xmTimelineMapper, tenantPropertiesService, sortProcessor);
-    }
-
-    @Primary
-    @Bean
-    static MethodSecurityExpressionHandler expressionHandler(XmPermissionEvaluator customPermissionEvaluator) {
-        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
-        expressionHandler.setPermissionEvaluator(customPermissionEvaluator);
-        return expressionHandler;
     }
 }
